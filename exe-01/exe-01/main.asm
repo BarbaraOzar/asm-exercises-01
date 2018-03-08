c1bb02481d4b29a172161000a8cbddde3dc9d59d
BarbaraOzar <barbara.ozar@gmail.com>
2018-03-08 13:42:40 +0100

    exe 1

--- exe-01/exe-01/main.asm
+++ exe-01/exe-01/main.asm
@@ -2,11 +2,12 @@
 ; exe-01.asm
 ;
 ; Created: 2018-03-08 11:53:50
-; Author : Dell
+; Author : Barbara
 ;
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+; Which registers can we load data into with LDI? 
+; Registers 16 to 31
+; Explain how this is related to the 16-bit Opcode for LDI.
+; There are only 4-bits avaliable for a Register address, therefore 
+; we can diffrenciate only between 16 different numbers.
