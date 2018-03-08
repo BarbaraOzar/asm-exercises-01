2ae898ab5d60e7c69f7514eef235342987a55dfb
BarbaraOzar <barbara.ozar@gmail.com>
2018-03-08 21:22:43 +0100

    Update main.asm

--- exe-05/exe-05/main.asm
+++ exe-05/exe-05/main.asm
@@ -14,11 +14,11 @@
 	ldi r19, 3			; loads value to r19
 	ldi r20, 33			; loads value to r20
 	nop
-	mul r19, r20		; mulitplies values from r19 and r20, the low bits of the result are placed in r0, the high ones in r1
+	mul r19, r20			; mulitplies values from r19 and r20, the low bits of the result are placed in r0, the high ones in r1
 	nop
-	sub r17, r18		; substracts r17 - r18, places the result in r17
+	sub r17, r18			; substracts r17 - r18, places the result in r17
 	nop
 	add r17, r0			; adds the results of substraction and multiplication, places result in r17
 	nop
-	add r16, r17		; adds r16 and r17, places result in r16
+	add r16, r17			; adds r16 and r17, places result in r16
 	nop
