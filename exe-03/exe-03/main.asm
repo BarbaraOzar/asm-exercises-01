ccea8de68822a8a7ae624f4e83f24bcaa315f001
BarbaraOzar <barbara.ozar@gmail.com>
2018-03-08 21:22:07 +0100

    Update main.asm

--- exe-03/exe-03/main.asm
+++ exe-03/exe-03/main.asm
@@ -10,9 +10,9 @@
 ; put the hexadecimal value 33 into register R17
 ; put the binary value 10110010 into register R18
 
-	ldi r16, 42				; load value into register r16
-	nop						; does nothing, but provides a place to put a breakpoint
+	ldi r16, 42			; load value into register r16
+	nop				; does nothing, but provides a place to put a breakpoint
 	ldi r17, 0x33			; load value into register r17
 	nop
-	ldi r18, 0b1011_0010	; load value into register r18
+	ldi r18, 0b1011_0010		; load value into register r18
 	nop
